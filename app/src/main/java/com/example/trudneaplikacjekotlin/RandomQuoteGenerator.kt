4857a6a435b242ad5b1b0029ad8f91b336613859
Maks <dawidjasperwojcik@gmail.com>
2024-12-16 10:46:30 +0100

    fourth commit

--- app/src/main/java/com/example/trudneaplikacjekotlin/RandomQuoteGenerator.kt
+++ app/src/main/java/com/example/trudneaplikacjekotlin/RandomQuoteGenerator.kt
@@ -15,4 +15,21 @@ class Quotes {
     fun addQuote(quote: String) {
         quotes.add(quote)
     }
+
+    companion object {
+        private var instance: Quotes? = null
+        fun getInstance(): Quotes {
+            if (instance == null) {
+                instance = Quotes()
+            }
+            return instance!!
+        }
+    }
+
+}
+fun main(){
+    val quotes = Quotes.getInstance()
+    println(quotes.getRandomQuotes())
+    quotes.addQuote("Jakis nowy cytat")
+    println(quotes.getRandomQuotes())
 }
